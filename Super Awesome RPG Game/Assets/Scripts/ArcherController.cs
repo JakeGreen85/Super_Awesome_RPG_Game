801c469435aebc98f854df8890213ae690414525
JakeGreen85 <plc403@alumni.ku.dk>
2023-10-21 23:25:46 +0200

    Added controller for archer class

---
+++ Super Awesome RPG Game/Assets/Scripts/ArcherController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+/// <summary>
+/// Controller for the archer class with all the warrior specific behavior
+/// </summary>
+public class ClassTemplate : CharacterController
+{
+    [Header("Archer Ability Settings")]
+
+    [Space(3)]
+
+    [Header("Ability 1")]
+    
+
+    [Space(5)]
+
+    [Header("Ability 2")]
+    
+
+    [Space(5)]
+
+    [Header("Ability 3")]
+    
+
+    [Space(5)]
+
+    [Header("Ability 4")]
+    
+
+
+    /// <summary>
+    /// Checks the input from the user and casts the corresponding ability
+    /// </summary>
+    /// <param name="ability">The ability index which will be cast (1-4)</param>
+    public override void UseAbility(int ability)
+    {
+        switch(ability)
+        {
+            case 1:
+                Ability1();
+                break;
+            case 2:
+                Ability2();
+                break;
+            case 3:
+                Ability3();
+                break;
+            case 4:
+                Ability4();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Ability1()
+    {
+        
+    }
+
+    private IEnumerator EnumForAbility1()
+    {
+        
+    }
+
+    private void Ability2()
+    {
+
+    }
+
+    private IEnumerator EnumForAbility2()
+    {
+        
+    }
+
+    private void Ability3()
+    {
+
+    }
+
+    private IEnumerator EnumForAbility3()
+    {
+        
+    }
+
+    private void Ability4()
+    {
+        
+    }
+
+    private IEnumerator EnumForAbility4()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision other) 
+    {
+
+    }
+
+}
